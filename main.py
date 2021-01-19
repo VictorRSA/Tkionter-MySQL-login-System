87426ab94e512a9925daa4f0bcd6212fe339dcac
Victor Nkuna <victor.nkuna@yahoo.com>
2021-01-19 16:18:03 +0200

    Tkinter login system

---
+++ main.py
@@ -0,0 +1,12 @@
+import login_system
+from login_system import main_account_screen
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main_account_screen() # call the main_account_screen() function
+    login_system.mainloop() # start the GUI
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
